99c40968a09bb13231cd26aa77f39ebe80d60ac7
NicolasVuillamy <nicolas.vuillamy@gmail.com>
2020-11-10 22:36:47 +0100

    Fix bash

--- tests/make_git_repo.bash
+++ tests/make_git_repo.bash
@@ -6,25 +6,25 @@
 # files over several commits.  The repo is used to
 # exercise some of cloc's git features.
 
-G=`which git`
-if test ! $G ; then 
+G=$(which git)
+if test ! "$G"; then
     echo "git is unavailable, exit."
     exit
 fi
 
-function git_init_config {                                  # {{{
+function git_init_config() { # {{{
     reponame=$1
     git init --quiet "${reponame}"
-    cd "${reponame}"
+    cd "${reponame}" || exit
     # explicitly don't use --global; settings for this repo only
     git config user.email "cloc.tester@github.com"
-    git config user.name  "Cloc Tester"
+    git config user.name "Cloc Tester"
 }
 # 1}}}
-function create_main_c {                                    # {{{
-fname=$1
-N=$2
-cat <<EO_001 > "${fname}"
+function create_main_c() { # {{{
+    fname=$1
+    N=$2
+    cat <<EO_001 >"${fname}"
 /*
  *               gcc hello.c -o hello
  */
@@ -35,27 +35,27 @@ main (int argc, char *argv[])
 EO_001
 }
 # 1}}}
-function create_py {                                        # {{{
-fname=$1
-cat <<EO_002 > "${fname}"
+function create_py() { # {{{
+    fname=$1
+    cat <<EO_002 >"${fname}"
 #!/usr/bin/env python
 
 print('hi')
 EO_002
 }
 # 1}}}
-function mod_py_A {                                         # {{{
-fname=$1
-cat <<EO_003 > "${fname}"
+function mod_py_A() { # {{{
+    fname=$1
+    cat <<EO_003 >"${fname}"
 #!/usr/bin/env python
 print('hi')
 print('hello')
 EO_003
 }
 # 1}}}
-function mod_py_B {                                         # {{{
-fname=$1
-cat <<EO_004 > "${fname}"
+function mod_py_B() { # {{{
+    fname=$1
+    cat <<EO_004 >"${fname}"
 #!/usr/bin/env python
 
 # five
@@ -65,7 +65,7 @@ for i in range(5):
 EO_004
 }
 # 1}}}
-function git_add_commit {                                   # {{{
+function git_add_commit() { # {{{
     fname=$1
     comment=$2
     taglabel=$3
@@ -74,7 +74,7 @@ function git_add_commit {                                   # {{{
     git tag "${taglabel}"
 }
 # 1}}}
-function git_rm_commit {                                    # {{{
+function git_rm_commit() { # {{{
     fname=$1
     comment=$2
     taglabel=$3
@@ -96,27 +96,27 @@ F4="weird'name.c"
 F5='ls -l .c'
 F6='Weird"Name.c'
 
-create_main_c  $F1  1
+create_main_c $F1 1
 create_main_c "$F2" 2
-create_main_c  $F3  3
-create_main_c  $F4  4
+create_main_c $F3 3
+create_main_c $F4 4
 create_main_c "$F5" 5
-create_main_c  $F6  6
-git_add_commit  $F1  "added $F1"  "Tag1"
-    git add 'date;ls ?.c'
-    git commit --quiet -m "${comment}" 'date;ls ?.c'
-    git tag "Tag2"
-git_add_commit  $F3  "added $F3"  "Tag3"
-git_add_commit  $F4  "added $F4"  "Tag4"
-    git add 'ls -l .c'
-    git commit --quiet -m "${comment}" 'ls -l .c'
-    git tag "Tag5"
-git_add_commit  $F6  "added $F6"  "Tag6"
+create_main_c $F6 6
+git_add_commit $F1 "added $F1" "Tag1"
+git add 'date;ls ?.c'
+git commit --quiet -m "${comment}" 'date;ls ?.c'
+git tag "Tag2"
+git_add_commit $F3 "added $F3" "Tag3"
+git_add_commit $F4 "added $F4" "Tag4"
+git add 'ls -l .c'
+git commit --quiet -m "${comment}" 'ls -l .c'
+git tag "Tag5"
+git_add_commit $F6 "added $F6" "Tag6"
 
-create_py      hello.py
-git_add_commit hello.py "add hello.py"              "T2"
-mod_py_A       hello.py
-git_add_commit hello.py "mod A"                     "T3"
-mod_py_B       hello.py
-git_add_commit hello.py "mod B"                     "T4"
-git_rm_commit  main.c   "using Python instead of C" "T5"
+create_py hello.py
+git_add_commit hello.py "add hello.py" "T2"
+mod_py_A hello.py
+git_add_commit hello.py "mod A" "T3"
+mod_py_B hello.py
+git_add_commit hello.py "mod B" "T4"
+git_rm_commit main.c "using Python instead of C" "T5"
